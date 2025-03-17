3f5035a2c57eb6f940ede4ffb5be66a81cd3dc6d
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-03-17 15:19:48 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,7 +41,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1.9).pow(player.multi.points)
+	let gain = new Decimal(1.9).pow(player.exp.points).mul(player.inf.points.add(1)).mul(player.eter.points.add(1).pow(2))
 	return gain
 }
 
