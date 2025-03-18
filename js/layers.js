06c36c715b6bee96de11bb050c6e74608071fddd
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-03-18 12:51:10 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,3 @@
-// clastas
 addLayer("exp", {
 	name: "exponent", // This is optional, only used in a few places, If absent it just uses the layer id.
 	symbol: "↑", // This appears on the layer's node. Default is the id with the first letter capitalized
