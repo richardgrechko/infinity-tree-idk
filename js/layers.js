91821609bf052e31d695a9e7b992fb74341a7756
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-03-18 12:37:25 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -112,3 +112,31 @@ addLayer("eter", {
 	],
 	layerShown(){return true}
 })
+addLayer("quantum", {
+	name: "quantum", // This is optional, only used in a few places, If absent it just uses the layer id.
+	symbol: "◻", // This appears on the layer's node. Default is the id with the first letter capitalized
+	position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+	startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+	}},
+	color: "#a040c7",
+	requires: new Decimal(2).pow(32).sub(1), // Can be a function that takes requirement increases into account
+	resource: "◻", // Name of prestige currency
+	baseResource: "∑", // Name of resource prestige is based on
+	baseAmount() {return player.eter.points}, // Get the current amount of baseResource
+	type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+	exponent: 0.5, // Prestige currency exponent
+	gainMult() { // Calculate the multiplier for main currency from bonuses
+	    mult = new Decimal(1)
+	    return mult
+	},
+	gainExp() { // Calculate the exponent on main currency from bonuses
+	    return new Decimal(16)
+	},
+	row: 2, // Row the layer is in on the tree (0 is the first row)
+	hotkeys: [
+	    {key: "q", description: "Q: Reset for quantums", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+	],
+	layerShown(){return true}
+})
