ff0a43f38521fd1d050d6ea3d8ea60b09a7f24c3
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-03-26 18:56:13 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("exp", {
-	name: "exponent", // This is optional, only used in a few places, If absent it just uses the layer id.
-	symbol: "↑", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("mul", {
+	name: "multiplier", // This is optional, only used in a few places, If absent it just uses the layer id.
+	symbol: "×", // This appears on the layer's node. Default is the id with the first letter capitalized
 	position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
 	startData() { return {
         	unlocked: true,
@@ -8,7 +8,7 @@ addLayer("exp", {
 	}},
 	color: "#dfbd92",
 	requires: new Decimal(10), // Can be a function that takes requirement increases into account
-	resource: "exponentiators", // Name of prestige currency
+	resource: "multipliers", // Name of prestige currency
 	baseResource: "points", // Name of resource prestige is based on
 	baseAmount() {return player.points}, // Get the current amount of baseResource
 	type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
